66533e82260857db39140ed95580e93502eea6c5
Maaz Khan <mmaazkhan2107@gmail.com>
2024-05-03 10:50:22 +0500

    clerk authentication integration completed

---
+++ app/(dashboard)/(routes)/(root)/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const RootPage = (props: Props) => {
+  return (
+    <div className='text-3xl text-green-500 font-bold'>RootPage</div>
+  )
+}
+
+export default RootPage
