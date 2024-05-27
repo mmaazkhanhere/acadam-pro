14adc8215b30d0785e4819be8446b6fde0894d2c
Mian Maaz Ullah Khan <mmaazkhan2107@gmail.com>
2024-05-27 21:30:20 +0500

    removing the is ready status from the video player

--- app/(dashboard)/(routes)/course/[courseId]/chapter/[chapterId]/_components/video-player.tsx
+++ app/(dashboard)/(routes)/course/[courseId]/chapter/[chapterId]/_components/video-player.tsx
@@ -16,26 +16,13 @@ type Props = {
 
 const VideoPlayer = ({title, playbackId, chapterId, courseId, isLocked, completeOnEnd, nextChapterId}: Props) => {
     
-    const [isReady, setIsReady] = useState<boolean>(false)
-
-    console.log(isReady)
-    
     return (
         <div className='relative aspect-video'>
-            {
-                !isReady  &&  <div
-                className='absolute inset-0 flex items-center justify-center bg-slate-800'>
-                    <Loader2
-                        className='w-8 h-8 animate-spin text-secondary'
-                    />
-                </div>
-            }
+
             <MuxPlayer
                 title={title}
                 playbackId={playbackId}
-                className={`${!isReady && 'hidden'}`}
                 autoPlay
-                onCanPlay={()=>setIsReady(true)}
             />
         </div>
     )
