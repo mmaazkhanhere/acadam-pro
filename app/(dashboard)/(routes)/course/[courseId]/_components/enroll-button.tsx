3f6ef159f6c2abb47090733d4e6d5c3fed11c859
Mian Maaz Ullah Khan <mmaazkhan2107@gmail.com>
2024-05-29 22:02:58 +0500

    course description ui completed. Next complete the review card to display reviews

---
+++ app/(dashboard)/(routes)/course/[courseId]/_components/enroll-button.tsx
@@ -0,0 +1,51 @@
+"use client";
+import React from "react";
+
+import { Button } from "@/components/ui/button";
+import { Chapter, Course } from "@prisma/client";
+import axios from "axios";
+import { redirect } from "next/navigation";
+import { useToast } from "@/components/ui/use-toast";
+
+type Props = {
+	course: Course & { chapters: Chapter[] };
+};
+
+const EnrollButton = ({ course }: Props) => {
+	const { toast } = useToast();
+
+	const onEnroll = async () => {
+		try {
+			if (course.isFree) {
+				await axios.patch(`/api/course/${course.id}/enroll`);
+				toast({
+					title: "Successful Enrolled",
+				});
+				redirect(
+					`/course/${course.id}/chapter/${course.chapters[0].id}`
+				);
+			} else {
+				if (course.isPro) {
+					toast({
+						title: "Subscribe to enroll",
+						variant: "destructive",
+					});
+				} else {
+					toast({
+						title: "Buy the course",
+						variant: "destructive",
+					});
+				}
+			}
+		} catch (error) {
+			toast({
+				title: "Something went wrong",
+				variant: "destructive",
+			});
+		}
+	};
+
+	return <Button onClick={onEnroll}>Enroll</Button>;
+};
+
+export default EnrollButton;
--- app/(dashboard)/(routes)/course/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/course/[courseId]/page.tsx
@@ -1,9 +1,73 @@
 import React from "react";
+import axios from "axios";
+import Link from "next/link";
+import { redirect } from "next/navigation";
 
-type Props = {};
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { useToast } from "@/components/ui/use-toast";
 
-const CoursePage = (props: Props) => {
-	return <div>CoursePage</div>;
+import prismadb from "@/lib/prismadb";
+
+import { ArrowLeft } from "lucide-react";
+import EnrollButton from "./_components/enroll-button";
+
+type Props = {
+	params: {
+		courseId: string;
+	};
+};
+
+const CoursePage = async ({ params }: Props) => {
+	const course = await prismadb.course.findUnique({
+		where: {
+			id: params.courseId,
+		},
+		include: {
+			chapters: true,
+			reviews: true,
+		},
+	});
+
+	if (!course) {
+		redirect("/");
+	}
+
+	return (
+		<section className="p-4">
+			<Button variant="outline" size="sm">
+				<Link
+					href={`/student/explore`}
+					className="flex items-center gap-x-2"
+				>
+					<ArrowLeft className="w-5 h-5" />
+					<p className="hidden md:block">Explore</p>
+				</Link>
+			</Button>
+
+			<div className="flex flex-col items-start mt-5 gap-y-2">
+				<div className="flex items-center justify-between max-w-7xl w-full">
+					<h2 className="text-2xl font-bold">{course.title}</h2>
+					<EnrollButton course={course} />
+				</div>
+
+				<Separator />
+
+				<p className="my-2">{course.description}</p>
+
+				<Separator />
+
+				<div className="flex flex-col items-start gap-y-2 mt-10">
+					<h2 className="text-2xl font-bold">Reviews</h2>
+					{course.reviews.length == 0 ? (
+						<p className="text-sm text-gray-500">No reviews yet</p>
+					) : (
+						<p>Reviews</p>
+					)}
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default CoursePage;
