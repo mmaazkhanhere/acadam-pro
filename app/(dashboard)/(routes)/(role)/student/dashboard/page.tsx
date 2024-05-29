3b48cc80fdfc56910220fb12438cc256a42ee3c9
Mian Maaz Ullah Khan <mmaazkhan2107@gmail.com>
2024-05-29 20:33:06 +0500

    securing routes accessible to student only

--- app/(dashboard)/(routes)/(role)/student/dashboard/page.tsx
+++ app/(dashboard)/(routes)/(role)/student/dashboard/page.tsx
@@ -1,37 +1,41 @@
-import { auth } from "@clerk/nextjs/server"
+import { auth } from "@clerk/nextjs/server";
 
-import StudentWelcomeBoard from "./_components/student-welcome-board"
+import StudentWelcomeBoard from "./_components/student-welcome-board";
 
-import prismadb from '@/lib/prismadb'
-import StudentOverview from "./_components/student-overview"
-import EnrolledCourseTable from "./_components/enrolled-courses-table"
+import prismadb from "@/lib/prismadb";
+import StudentOverview from "./_components/student-overview";
+import EnrolledCourseTable from "./_components/enrolled-courses-table";
+import { isAdmin, isTeacher } from "@/helpers/userCheck";
+import { redirect } from "next/navigation";
 
-
-type Props = {}
+type Props = {};
 
 const StudentDashboard = async (props: Props) => {
+	const { userId } = auth();
+	const admin = await isAdmin(userId as string);
+	const teacher = await isTeacher(userId as string);
 
-    const { userId } = auth();
+	if (!userId || teacher || admin) {
+		redirect("/");
+	}
 
-    const user = await prismadb.user.findUnique({
-        where: {
-            id: userId as string
-        },
-        select: {
-            name: true
-        }
-    })
+	const user = await prismadb.user.findUnique({
+		where: {
+			id: userId as string,
+		},
+		select: {
+			name: true,
+		},
+	});
 
-    return (
-        <div className="p-4">
-            <StudentWelcomeBoard
-                name={user?.name as string}
-            />
-            <StudentOverview />
+	return (
+		<div className="p-4">
+			<StudentWelcomeBoard name={user?.name as string} />
+			<StudentOverview />
 
-            <EnrolledCourseTable />
-        </div>
-    )
-}
+			<EnrolledCourseTable />
+		</div>
+	);
+};
 
-export default StudentDashboard
+export default StudentDashboard;
